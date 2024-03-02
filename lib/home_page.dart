37c8b1b48b8bfbf96299a9834852ba1fbd591574
Sunlution <sunface@outlook.kr>
2024-03-02 17:02:05 +0900

    [UPDATE] localization(system)

---
+++ lib/home_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Internationalization'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  AppLocalizations.of(context)!.systemLanguage,
+                ),
+                const Text(
+                 ":",
+                ),
+                Text(
+                  AppLocalizations.of(context)!.language,
+                ),
+              ],
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
---
+++ lib/l10n/l10n.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/material.dart';
+
+class L10n {
+  static final all = <Locale>[
+    const Locale('en', 'US'),
+    const Locale('ko', 'KR'),
+    const Locale('zh', 'CN'),
+    const Locale('ja', 'JP'),
+    const Locale('ru', 'RU'),
+  ];
+}
